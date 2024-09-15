3339398871b638f52df6ec4680dac58ab19fa948
Mark Post <mark.post@live.nl>
2024-09-15 20:40:59 +0200

    Updated properties Updated included mvn Updated unit test

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker-compose down --volumes
-docker-compose up -d
+docker compose down --volumes
+docker compose up -d
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker system prune
-docker-compose build --no-cache
-docker-compose up -d
+docker compose build --no-cache
+docker compose up -d
